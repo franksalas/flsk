178f329dcbe80e6c665a51d6a5afda6cb39f8520
Francisco Salas <frank.salas@gmail.com>
2018-02-26 14:25:05 -0600

    end of chapter 2

--- flsk.py
+++ flsk.py
@@ -1,12 +1,9 @@
-from flask import Flask
-
-app = Flask(__name__)
+from app import app
 
 
-@app.route('/')
-def hello_world():
-    return 'Sup Globe!!'
 
+
+## NO TOChy
 
 if __name__ == '__main__':
     app.run()
